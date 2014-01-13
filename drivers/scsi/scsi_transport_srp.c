78ebde397d68c5bffcf2dfe9d64a7f3dd1113294
Bart Van Assche <bvanassche@acm.org>
2014-01-13 12:39:35 +0100

    scsi_transport_srp: Fix kernel-doc warnings
    The following command has been used to verify that the kernel-doc
    tool no longer complains about undocumented fields:
    
        scripts/kernel-doc -html drivers/scsi/scsi_transport_srp.c \
          include/scsi/scsi_transport_srp.h >srp-transport-doc.html
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Acked-by: Sebastian Riemer <sebastian.riemer@profitbricks.com>
    Acked-by: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Roland Dreier <roland@purestorage.com>

--- drivers/scsi/scsi_transport_srp.c
+++ drivers/scsi/scsi_transport_srp.c
@@ -85,6 +85,9 @@ static inline struct Scsi_Host *rport_to_shost(struct srp_rport *r)
 
 /**
  * srp_tmo_valid() - check timeout combination validity
+ * @reconnect_delay: Reconnect delay in seconds.
+ * @fast_io_fail_tmo: Fast I/O fail timeout in seconds.
+ * @dev_loss_tmo: Device loss timeout in seconds.
  *
  * The combination of the timeout parameters must be such that SCSI commands
  * are finished in a reasonable time. Hence do not allow the fast I/O fail
@@ -489,6 +492,7 @@ invalid:
 
 /**
  * srp_reconnect_work() - reconnect and schedule a new attempt if necessary
+ * @work: Work structure used for scheduling this operation.
  */
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 18)
 static void srp_reconnect_work(void *arg)
@@ -535,6 +539,7 @@ static void __rport_fail_io_fast(struct srp_rport *rport)
 
 /**
  * rport_fast_io_fail_timedout() - fast I/O failure timeout handler
+ * @work: Work structure used for scheduling this operation.
  */
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 18)
 static void rport_fast_io_fail_timedout(void *arg)
@@ -559,6 +564,7 @@ static void rport_fast_io_fail_timedout(struct work_struct *work)
 
 /**
  * rport_dev_loss_timedout() - device loss timeout handler
+ * @work: Work structure used for scheduling this operation.
  */
 #if LINUX_VERSION_CODE <= KERNEL_VERSION(2, 6, 18)
 static void rport_dev_loss_timedout(void *arg)
@@ -619,6 +625,7 @@ static void __srp_start_tl_fail_timers(struct srp_rport *rport)
 
 /**
  * srp_start_tl_fail_timers() - start the transport layer failure timers
+ * @rport: SRP target port.
  *
  * Start the transport layer fast I/O failure and device loss timers. Do not
  * modify a timer that was already started.
@@ -633,6 +640,7 @@ EXPORT_SYMBOL(srp_start_tl_fail_timers);
 
 /**
  * scsi_request_fn_active() - number of kernel threads inside scsi_request_fn()
+ * @shost: SCSI host for which to count the number of scsi_request_fn() callers.
  */
 static int scsi_request_fn_active(struct Scsi_Host *shost)
 {
@@ -657,6 +665,7 @@ static int scsi_request_fn_active(struct Scsi_Host *shost)
 
 /**
  * srp_reconnect_rport() - reconnect to an SRP target port
+ * @rport: SRP target port.
  *
  * Blocks SCSI command queueing before invoking reconnect() such that
  * queuecommand() won't be invoked concurrently with reconnect() from outside
@@ -735,6 +744,7 @@ EXPORT_SYMBOL(srp_reconnect_rport);
 #if LINUX_VERSION_CODE > KERNEL_VERSION(2, 6, 18)
 /**
  * srp_timed_out() - SRP transport intercept of the SCSI timeout EH
+ * @scmd: SCSI command.
  *
  * If a timeout occurs while an rport is in the blocked state, ask the SCSI
  * EH to continue waiting (BLK_EH_RESET_TIMER). Otherwise let the SCSI core
@@ -807,6 +817,7 @@ static int srp_host_match(struct attribute_container *cont, struct device *dev)
 
 /**
  * srp_rport_get() - increment rport reference count
+ * @rport: SRP target port.
  */
 void srp_rport_get(struct srp_rport *rport)
 {
@@ -816,6 +827,7 @@ EXPORT_SYMBOL(srp_rport_get);
 
 /**
  * srp_rport_put() - decrement rport reference count
+ * @rport: SRP target port.
  */
 void srp_rport_put(struct srp_rport *rport)
 {

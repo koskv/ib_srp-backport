0cb86d3de9cb9f4cf804291ec934e2ea16a0ee8a
Bart Van Assche <bart.vanassche@sandisk.com>
2015-07-24 19:49:45 -0700

    IB/srp: Bump driver version and release date

--- drivers/infiniband/ulp/srp/ib_srp.c
+++ drivers/infiniband/ulp/srp/ib_srp.c
@@ -63,10 +63,10 @@
 #define DRV_NAME	"ib_srp"
 #define PFX		DRV_NAME ": "
 #ifndef DRV_VERSION
-#define DRV_VERSION	"1.0"
+#define DRV_VERSION	"2.0"
 #endif
 #ifndef DRV_RELDATE
-#define DRV_RELDATE	"July 1, 2013"
+#define DRV_RELDATE	"July 26, 2015"
 #endif
 
 MODULE_AUTHOR("Roland Dreier");
